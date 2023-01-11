68f3cb7bc96a06b3317927198a6d98d1f5d3d327
Zer02 <abasdeo29@gmail.com>
2023-01-10 23:58:53 -0500

    added escrow contract

--- test/Escrow.js
+++ test/Escrow.js
@@ -1,10 +1,55 @@
-const { expect } = require('chai');
-const { ethers } = require('hardhat');
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
 
 const tokens = (n) => {
-    return ethers.utils.parseUnits(n.toString(), 'ether')
-}
+  return ethers.utils.parseUnits(n.toString(), "ether");
+};
 
-describe('Escrow', () => {
+describe("Escrow", () => {
+  let buyer, seller, inspector, lender;
+  let realEstate, escrow;
 
-})
+	beforeEach(async () => {
+    // Set up accounts
+    [buyer, seller, inspector, lender] = await ethers.getSigners();
+
+    // Deploy Real Estate
+    const RealEstate = await ethers.getContractFactory("RealEstate");
+    realEstate = await RealEstate.deploy();
+
+    // Mint
+    let transaction = await realEstate.connect(seller).mint("https://ipfs.io/ipfs/QmTudSYeM7mz3PkYEWXWqPjomRPHogcMFSq7XAvsvsgAPS");
+    await transaction.wait();
+
+    const Escrow = await ethers.getContractFactory("Escrow");
+    escrow = await Escrow.deploy(
+      realEstate.address,
+      seller.address,
+      inspector.address,
+      lender.address
+    );
+	});
+  
+	describe('Deployment', () => {
+
+			it('Returns NFT address', async () => {
+				const result = await escrow.nftAddress();
+				expect(result).to.be.equal(realEstate.address);
+			});
+		
+			it('Returns seller', async () => {
+				const result = await escrow.seller();
+				expect(result).to.be.equal(seller.address);
+			});
+		
+			it('Returns inspector', async () => {
+				const result = await escrow.inspector();
+				expect(result).to.be.equal(inspector.address);
+			});
+		
+			it('Returns lender', async () => {
+				const result = await escrow.lender();
+				expect(result).to.be.equal(lender.address);
+			});
+	})
+});
